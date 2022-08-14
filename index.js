eb9e0115e71ba1b98a7b07440f5f9bdc168c30f6
trevor dewindt <trevor.dewindt@gmail.com>
2022-08-13 21:11:21 -0600

    commit tree 926d45c1e809f6f1c3c9639a2bcb4d43165c65c7

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+// const init = () => {
+
+function submitData(userName, userEmail) {
+    const userObject = {
+        name: userName,
+        email: userEmail,
+    };
+
+    const p = document.createElement('p');
+    const body = document.querySelector('body');
+
+    const fetchRequest = () => fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(userObject),
+    })
+    .then(res => res.json())
+    .then(data => {
+        p.textContent = data.id;
+        body.appendChild(p);   
+    })
+    .catch(error => {
+        p.textContent = error.message;
+        body.appendChild(p);
+    });
+
+    return fetchRequest();
+}
+
+    // submitData('kevin6', 'keviniscool@kevin.com6');
+// }
+
+
+// document.addEventListener('DOMContentLoaded', init);
